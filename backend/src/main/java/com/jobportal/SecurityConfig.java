74e37f4b8ab80d24986bac64802e18a5da7d5331
Anamika-30 <anamikajha302003@gmail.com>
2025-05-15 03:57:33 -0700

    SecurityConfig.java

--- backend/src/main/java/com/jobportal/SecurityConfig.java
+++ backend/src/main/java/com/jobportal/SecurityConfig.java
@@ -1,42 +1,22 @@
 package com.jobportal;
 
-
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
-import com.jobportal.jwt.JwtAuthenticationEntryPoint;
-import com.jobportal.jwt.JwtAuthenticationFilter;
-
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-    @Autowired
-    private JwtAuthenticationEntryPoint point;
-    @Autowired
-    private JwtAuthenticationFilter filter;
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-
-        http.csrf(csrf -> csrf.disable())
-                .authorizeRequests().
-                requestMatchers("/auth/login","/users/register", "/users/verifyOtp/**","/users/sendOtp/**","/users/changePass").permitAll()
-                .anyRequest()
-                .authenticated()
-                .and().exceptionHandling(ex -> ex.authenticationEntryPoint(point))
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-        http.addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class);
+        http
+            .csrf(csrf -> csrf.disable())
+            .authorizeRequests()
+            .anyRequest()
+            .permitAll();  // <-- This allows access to all endpoints without authentication
         return http.build();
     }
-
-
 }
